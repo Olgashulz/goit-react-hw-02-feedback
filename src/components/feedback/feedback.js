b4b83ea06b464db3a33779a1fc1f17cd3d1a386d
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-09-28 16:25:45 +0300

    1 этап

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
 
 class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = event => {
+    const value = event.target.textContent.toLowerCase();
+    this.setState(prevState => ({
+      ...prevState,
+      [value]: prevState[value] + 1,
+    }));
+  };
+
   render() {
-    return <div className="Feedback">Hello!!!</div>;
+    return (
+      <div className="Feedback">
+        <h3>Please leave feedback</h3>
+        <div className="buttonsContainer">
+          <button type="button" onClick={this.addFeedback}>
+            Good
+          </button>
+          <button type="button" onClick={this.addFeedback}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.addFeedback}>
+            Bad
+          </button>
+        </div>
+        <div className="Statistics">
+          <ul className="list">
+            <li>
+              Good
+              <span>{this.state.good}</span>
+            </li>
+            <li>
+              Neutral
+              <span>{this.state.neutral}</span>
+            </li>
+            <li>
+              Bad
+              <span>{this.state.bad}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
