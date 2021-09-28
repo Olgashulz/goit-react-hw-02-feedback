df577bdf02f068a091238ffba1cd5b14c398de23
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-09-28 12:37:17 +0300

    Настроила арр

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Feedback from './components/feedback/feedback';
+
+export default function App() {
+  return (
+    <div className="container">
+      <Feedback />
+    </div>
+  );
+}
---
+++ src/components/feedback/feedback.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+  render() {
+    return <div className="Feedback">Hello!!!</div>;
+  }
+}
+
+export default Feedback;
